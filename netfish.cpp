b07ce8a69e84f3e76b00662367a6af3b1f6a1635
AngieJC <htk90uggk@163.com>
2019-12-22 16:42:14 +0800

    完善了聊天功能，程序客户端退出后服务器将自动退出，不会占用系统资源

--- netfish.cpp
+++ netfish.cpp
@@ -6,18 +6,25 @@
 #include <pthread.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 
 #include "getopt.h"
 #include "global.h"
 
 using namespace std;
 
+typedef struct for_std
+{
+	int * clnt_sock_ptr;
+	pthread_t pid, other_pid;
+}for_std;
+
 int listen(char * ip, int port_start);
 void * std_local(void * clnt_sock);
 void * std_remote(void * clnt_sock);
-
-int stop = 0;
 
 int main(int argc, char ** argv)
 {
@@ -53,46 +60,39 @@ int listen(char * ip, int port_start)
 	struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_size = sizeof(clnt_addr);
     int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-	int * clnt_sock_ptr = &clnt_sock;
-	pthread_t local_pid, remote_pid;  // 定义两个线程的pid
-	pthread_create(&local_pid, NULL, std_local, (void *)clnt_sock_ptr);
-	pthread_create(&remote_pid, NULL, std_remote, (void *)clnt_sock_ptr);
-	pthread_join(local_pid,NULL);
-	pthread_join(remote_pid,NULL);
+	//int * clnt_sock_ptr = &clnt_sock;
+	//pthread_t local_pid, remote_pid;  // 定义两个线程的pid
+	for_std local, remote;
+	local.clnt_sock_ptr = &clnt_sock;
+	remote.clnt_sock_ptr = &clnt_sock;
+	pthread_create(&local.pid, NULL, std_local, (void *)&local);
+	pthread_create(&remote.pid, NULL, std_remote, (void *)&remote);
+	remote.other_pid = local.pid;
+	local.other_pid = remote.pid;
+	pthread_join(local.pid,NULL);
+	pthread_join(remote.pid,NULL);
 	close(serv_sock);
+	close(clnt_sock);
 	return 0;
-
-	/*
-
-    //接收客户端请求
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size = sizeof(clnt_addr);
-    int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-
-	//向客户端发送数据
-    char str[1024] = {0};
-	while(1)
-	{
-		read(clnt_sock, str, sizeof(str));
-		cout << str;
-		memset(str, 0, 1024);
-	}
-   
-    //关闭套接字
-    close(clnt_sock);
-    close(serv_sock);
-	*/
 }
 
 
 void * std_local(void * clnt_sock)
 {
+	for_std * sock = (for_std *)clnt_sock;
+	struct tcp_info info;
+	int len = sizeof(info);
 	char buff[1024];
 	memset(buff, 0, 1024);
-	int * sock = (int *)clnt_sock;
 	while(1)
 	{
-		read(*sock, buff, sizeof(buff));
+		getsockopt(*sock->clnt_sock_ptr, IPPROTO_TCP, TCP_INFO, (void *)&info, (socklen_t *)&len);
+		if(!(info.tcpi_state==TCP_ESTABLISHED))  // 连接已经断开
+		{
+			pthread_cancel(sock->other_pid);
+			break;
+		}
+		read(*sock->clnt_sock_ptr, buff, sizeof(buff));
 		cout << buff;
 		memset(buff, 0, 1024);
 	}
@@ -104,12 +104,12 @@ void * std_remote(void * clnt_sock)
 {
 	char buff[1024];
 	memset(buff, 0, 1024);
-	int * sock = (int *)clnt_sock;
+	for_std * sock = (for_std *)clnt_sock;
 	while(1)
 	{
 		cin >> buff;
 		sprintf(buff, "%s\r\n", buff);
-		write(*sock, buff, sizeof(buff));
+		write(*sock->clnt_sock_ptr, buff, sizeof(buff));
 		memset(buff, 0, 1024);
 	}
 	return NULL;
